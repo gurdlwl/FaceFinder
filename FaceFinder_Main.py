644e311bca97ad409ee642c7f7bf8933dabb94c7
gurdlwl <gurdlwl@gmail.com>
2019-06-05 14:46:39 +0900

    [UPDATE] button, fileDialog 추가

--- FaceFinder_Main.py
+++ FaceFinder_Main.py
@@ -1,7 +1,11 @@
 import sys
 
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QAction, qApp
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QAction, qApp, QLabel, QGridLayout, QPushButton, \
+    QFileDialog, QMessageBox
+from qtpy import QtGui
+
 
 class MyApp(QMainWindow):
 
@@ -12,7 +16,7 @@ class MyApp(QMainWindow):
     def initUI(self):
 
         exitAction = QAction('Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setShortcut('Esc')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
 
@@ -23,10 +27,29 @@ class MyApp(QMainWindow):
 
         self.setWindowTitle('Face Finder')
         self.setWindowIcon(QIcon('')) # Icon path
-        self.setGeometry(100, 100, 1280, 720) # move, resize를 합쳐놓은 것
+        self.setGeometry(100, 100, 1600, 900) # move, resize를 합쳐놓은 것
+        # self.setFixedSize(1680, 900) # 창 크기 고정
         # self.move(10, 10)
         # self.resize(1280, 720)
+
+        self.label = QLabel(self)
+        self.label.move(200, 200)
+
+        fileOpenBtn = QPushButton('Open Image File', self)
+        fileOpenBtn.move(800, 450)
+        fileOpenBtn.clicked.connect(self.fileOpenBtnClickMethod)
+
         self.show()
+
+    def fileOpenBtnClickMethod(self):
+        filename = QFileDialog.getOpenFileName(self, 'Open File', '/', 'Image Files(*.png *.jpg)')
+        print(filename)
+
+        if filename[0]:
+            pixmap = QtGui.QPixmap(filename[0])
+            self.label.setPixmap(QPixmap(pixmap))
+            self.label.resize(pixmap.width(), pixmap.height())
+        # QMessageBox.about(self, "notice", "click!")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
