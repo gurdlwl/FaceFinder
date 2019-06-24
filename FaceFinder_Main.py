c123835f508de3f45b01eb661e724b1f8593514a
gurdlwl <gurdlwl@gmail.com>
2019-06-24 11:47:19 +0900

    [UPDATE] Menubar 추가

--- FaceFinder_Main.py
+++ FaceFinder_Main.py
@@ -6,6 +6,9 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QAction, 
 from PyQt5 import QtGui, QtCore, QtWidgets
 from qtpy import QtGui
 import functools
+import dlib, cv2
+
+
 
 class MainWindow(QMainWindow):
 
@@ -22,21 +25,37 @@ class MainWindow(QMainWindow):
 
     def initUI(self):
 
-        '''
         exitAction = QAction('Exit', self)
         exitAction.setShortcut('Esc')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.)
+        exitAction.setStatusTip('Exit Application')
+        exitAction.triggered.connect(qApp.quit)
+
+        refreshAction = QAction('Refresh', self)
+        refreshAction.setShortcut('Ctrl+F5')
+        refreshAction.setStatusTip('Refresh Screen')
+        refreshAction.triggered.connect(self.setBaseImg)
+
+        detectAction = QAction('Face Detect Mode (Expected)', self)
+        detectAction.setShortcut('Ctrl+1')
+        detectAction.setStatusTip('Change Mode to Face Detect')
+        # detectAction.triggered.connect()
+
+        recognizeAction = QAction('Face Recognize Mode (Expected)', self)
+        recognizeAction.setShortcut('Ctrl+2')
+        recognizeAction.setStatusTip('Change Mode to Face Detect')
+        # recognizeAction.triggered.connect()
 
         menubar = self.menuBar()
-        menubar.setNativeMenuBar(False)
-        fileMenu = menubar.addMenu('File')
-        fileMenu.addAction(exitAction)
-        '''
+        Menu = menubar.addMenu('Menu')
+        Menu.addAction(exitAction)
+        Menu.addAction(refreshAction)
+        Mode = menubar.addMenu('Mode')
+        Mode.addAction(detectAction)
+        Mode.addAction(recognizeAction)
 
         self.mainImg = QLabel(self) # main사진이 들어갈 Label
         self.mainImg.setObjectName('mainImg')
-        self.mainImg.setMaximumSize(900, 600) # label 최대 width, height를 조절
+        self.mainImg.setMaximumSize(910, 910) # label 최대 width, height를 조절
         self.mainImg.setScaledContents(1) # Image를 label 크기에 맞게 조절. 1 : true, 0 : false. false인 경우, 이미지가 label 크기만큼만 나오고 잘린다.
 
         self.faceImg = QLabel(self) # 얼굴사진이 들어갈 Label
@@ -55,27 +74,24 @@ class MainWindow(QMainWindow):
         self.mainImg.mousePressEvent = functools.partial(MainWindow.fileOpenMethod, self.mainImg)
         self.faceImg.mousePressEvent = functools.partial(MainWindow.fileOpenMethod, self.faceImg)
 
-        # self.fileOpenBtn = QPushButton('Open Image File', self)
-        # self.fileOpenBtn.setMaximumWidth(150)
-        # self.fileOpenBtn.clicked.connect(self.fileOpenClickMethod)
-
-        # 일단 임시로 image 넣어놓음
-        baseImg = QtGui.QPixmap('D:/사진/캡처.PNG')
-        self.mainImg.setPixmap(baseImg)
-        self.faceImg.setPixmap(baseImg)
-        self.detectedFaceImg.setPixmap(baseImg)
+        self.setBaseImg()
 
         self.createGridLayout()
         self.setCentralWidget(self.horizontalGroupBox)
 
         self.setWindowTitle('Face Finder')
         self.setWindowIcon(QIcon(''))  # Icon path
-        # self.move(10, 10)
-        # self.resize(1280, 720)
         self.setGeometry(self.left, self.top, self.width, self.height)  # move, resize를 합쳐놓은 것
         self.setFixedSize(self.width, self.height) # 창 크기 고정
 
         self.show()
+
+    def setBaseImg(self):
+        # 일단 임시로 image 넣어놓음
+        baseImg = QtGui.QPixmap('D:/사진/캡처.PNG')
+        self.mainImg.setPixmap(baseImg)
+        self.faceImg.setPixmap(baseImg)
+        self.detectedFaceImg.setPixmap(baseImg)
 
 
     def fileOpenMethod(self, event):
@@ -91,6 +107,7 @@ class MainWindow(QMainWindow):
 
         if(str(self.objectName()) == 'mainImg') :
             print('hahaha I find it')
+            faceDetection.test(self, filename[0])
             # 해당 파일 넘겨줘서 face recognition
 
 
@@ -119,17 +136,15 @@ class MainWindow(QMainWindow):
         self.grid_layout2.addWidget(self.mainImg, 0, 0)
         self.grid_layout2.addLayout(self.grid_layout, 0, 1)
 
-        '''        
-        self.grid_layout = QGridLayout()
-        # grid_layout.addWidget( WIDGET, 행, 열 )
-        self.grid_layout.addWidget(self.mainImg, 0, 0)
-        self.grid_layout.addWidget(self.faceImg, 0, 1)
-        self.grid_layout.addWidget(self.detectedFaceImg, 1, 1)
-
-        self.horizontalGroupBox.setLayout(self.grid_layout)
-        '''
-
         self.horizontalGroupBox.setLayout(self.grid_layout2)
+
+
+
+class faceDetection():
+
+    def test(self, path):
+        print('hihi ' + path)
+
 
 
 def main():
@@ -137,6 +152,7 @@ def main():
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
